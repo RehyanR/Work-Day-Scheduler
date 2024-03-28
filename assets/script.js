577df77f28f96880ef93fd3bf92a7359f7bcbefc
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-28 16:17:32 +0000

    started adding function for generationg time blocks

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });
 
+for (var hour = 9; hour <= 17; hour++) {
+  var timeBlock = $("<div>").addClass("row time-block").attr("data-hour", hour);
+}
 
 function getLocalStorageKey(hour) {
   return "event_" + dayjs().format("YYYY-MM-DD") + "_" + hour;
