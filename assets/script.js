353200c75fa658fede1269cb6ef7e4aa775f1e74
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-28 15:13:51 +0000

    added function to generate local storage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });
+
+
+
+function getLocalStorageKey(hour) {
+    return "event_" + dayjs().format("YYYY-MM-DD") + "_" + hour;
+  }
